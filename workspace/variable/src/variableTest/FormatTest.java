63baa43f464a60032b0b009f4540b3ca343f4a5c
coder-zeorjun <jjoju2331@gmail.com>
2022-12-29 22:15:33 +0900

    print, variable study

---
+++ workspace/variable/src/variableTest/FormatTest.java
@@ -0,0 +1,22 @@
+package variableTest;
+
+public class FormatTest {
+
+	public static void main(String[] args) {
+		
+		String name = "한동석";	// 자료구조 및 초기화
+		int age = 20;
+		double height = 123.555;
+		
+		System.out.printf("이름 : %s\n", name);
+		System.out.printf("나이 : %d살", age);
+		System.out.printf("몸무게 : %.2fkg\n", height);	// 반올림
+		
+		
+		
+		
+		
+		
+		
+	}
+}
---
+++ workspace/variable/src/variableTest/StorageClassTest2.java
@@ -0,0 +1,36 @@
+package variableTest;
+
+public class StorageClassTest2 {
+
+	public static void main(String[] args) {
+		
+		StorageClassTest1 object = new StorageClassTest1();
+		
+		
+		object.increaseData_s();
+		object.increaseData_s();
+		object.increaseData_s();
+		object.increaseData_s();
+		object.increaseData_s();
+		object = new StorageClassTest1();
+		object.increaseData_s();
+		object.increaseData_s();
+		object.increaseData_s();
+		object.increaseData_s();
+		object.increaseData_s();
+		
+		
+		
+		
+		object.increaseData();
+		object.increaseData();
+		object.increaseData();
+		object.increaseData();
+		object = new StorageClassTest1();
+		object.increaseData();
+		object.increaseData();
+		object.increaseData();
+		object.increaseData();
+		object.increaseData();
+	}
+}
